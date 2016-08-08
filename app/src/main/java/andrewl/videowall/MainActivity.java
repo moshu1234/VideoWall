4a5bb3a9c1dac203544c72d0674c0fe15631fc4c
Andrew Liu <187032677@163.com>
2016-08-08 23:52:38 +0800

    add top bar and tab bar

--- app/src/main/java/andrewl/videowall/MainActivity.java
+++ app/src/main/java/andrewl/videowall/MainActivity.java
@@ -1,19 +1,30 @@
 package andrewl.videowall;
 
 import android.content.res.Configuration;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import andrewl.videowall.DataBase.greendao.VideoWallHelper;
 import andrewl.videowall.UI.GLView;
+import andrewl.videowall.UI.MyWidget.WidgetTopBar;
 import andrewl.videowall.UI.Renderer;
 import cn.easyar.engine.EasyAR;
+import me.majiajie.pagerbottomtabstrip.Controller;
+import me.majiajie.pagerbottomtabstrip.PagerBottomTabLayout;
+import me.majiajie.pagerbottomtabstrip.TabItemBuilder;
+import me.majiajie.pagerbottomtabstrip.TabLayoutMode;
+import me.majiajie.pagerbottomtabstrip.listener.OnTabItemSelectListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
     private VideoWallHelper mVideoWallHelper;
     static String key = "2P0RAiTyyHKRzSFXFVJa6A8B7yr7J60S0f9NSXsuY4xMPyP8pYwuq7nw4cklfJBOWc8djW086TPTHS1bxQbIGyw4GQoAX1BqURNK0730bcff48994f078af6f669f29a4904a9HNqd9BEIXAxjqQM2o46zQhDIyowT3JRtJth2CyjiE38RjbRnpG4QXlJC7oT1yHoEsW";
 
@@ -29,10 +40,16 @@ public class MainActivity extends AppCompatActivity {
     private native boolean nativeReinit();
     private native void nativeDestory();
     private native void nativeRotationChange(boolean portrait);
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar = getSupportActionBar();//高版本可以换成 ActionBar actionBar = getActionBar();
+        actionBar.hide();
+        BottomTabTest();
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON, WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         mVideoWallHelper = new VideoWallHelper().getInstance();
         mVideoWallHelper.initGreenDao(this,"T_VIDEOWALL");
@@ -84,5 +101,80 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void initTopbar(){
+        WidgetTopBar wtbOne = (WidgetTopBar) findViewById(R.id.wtb_curve);
+        wtbOne.getLeftBtnImage().setOnClickListener(this);
+        wtbOne.getRightBtnImage().setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ib_left_top_bar: {
+                Toast.makeText(this, "第二个标题 左边按钮", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.ib_right_top_bar: {
+                Toast.makeText(this, "第一个标题 右边按钮", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.btn_left_top_bar: {
+                Toast.makeText(this, "第一个标题 左边按钮", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.btn_right_top_bar: {
+                Toast.makeText(this, "第二个标题 右边按钮", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    }
+    OnTabItemSelectListener listener = new OnTabItemSelectListener() {
+        @Override
+        public void onSelected(int index, Object tag)
+        {
+            Log.i("asd","onSelected:"+index+"   TAG: "+tag.toString());
+
+//            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+//            //transaction.setCustomAnimations(R.anim.push_up_in,R.anim.push_up_out);
+//            transaction.replace(R.id.frameLayout,mFragments.get(index));
+//            transaction.commit();
+        }
+
+        @Override
+        public void onRepeatClick(int index, Object tag) {
+            Log.i("asd","onRepeatClick:"+index+"   TAG: "+tag.toString());
+        }
+    };
+    private void BottomTabTest()
+    {
+        Controller controller;
+//    int[] testColors = {0xFF7BA3A8,0xFFF4F3DE,0xFFBEAD92,0xFFF35A4A,0xFF5B4947};
+//    int[] testColors = {0xFF00796B,0xFF8D6E63,0xFF2196F3,0xFF607D8B,0xFFF57C00};
+        int[] testColors = {0xFF00796B,0xFF5B4947,0xFF607D8B,0xFFF57C00,0xFFF57C00};
+        PagerBottomTabLayout pagerBottomTabLayout = (PagerBottomTabLayout) findViewById(R.id.tab);
+
+        //用TabItemBuilder构建一个导航按钮
+        TabItemBuilder tabItemBuilder = new TabItemBuilder(this).create()
+                .setDefaultIcon(android.R.drawable.ic_menu_send)
+                .setText("信息")
+                .setSelectedColor(testColors[0])
+                .setTag("A")
+                .build();
+
+        //构建导航栏,得到Controller进行后续控制
+        controller = pagerBottomTabLayout.builder()
+                .addTabItem(tabItemBuilder)
+                .addTabItem(android.R.drawable.ic_menu_compass, "位置",testColors[1])
+                .addTabItem(android.R.drawable.ic_menu_search, "搜索",testColors[2])
+                .addTabItem(android.R.drawable.ic_menu_help, "帮助",testColors[3])
+//                .setMode(TabLayoutMode.HIDE_TEXT)
+//                .setMode(TabLayoutMode.CHANGE_BACKGROUND_COLOR)
+                .setMode(TabLayoutMode.HIDE_TEXT| TabLayoutMode.CHANGE_BACKGROUND_COLOR)
+                .build();
+
+//        controller.setMessageNumber("A",2);
+//        controller.setDisplayOval(0,true);
+
+        controller.addTabItemClickListener(listener);
     }
 }
